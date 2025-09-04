d2300a0533f14b9ebfa924050f40ceb5e7d69cc7
Yacine Kanaan <yacine.kanaan@edu.vs.ch>
2025-09-04 17:59:24 +0200

    fix(themes): add multi themes support

---
+++ themes/56kcloud/index.ts
@@ -0,0 +1,15 @@
+import './styles/index.ts'
+
+import Cover from './layouts/cover.vue'
+import Hero56k from './layouts/hero-56k.vue'
+import Reference from './layouts/reference.vue'
+import Intro from './layouts/intro.vue'
+
+export const layouts = {
+  cover: Cover,
+  'hero-56k': Hero56k,
+  reference: Reference,
+  intro: Intro,
+}
+
+export default { layouts }
---
+++ themes/teaching/layouts/cover.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TeachingDefault from './default.vue'
+</script>
+
+<template>
+    <TeachingDefault>
+        <div class="slidev-layout cover">
+          <div class="my-auto w-full">
+            <Logo /> 
+            <slot />
+          </div>
+        </div>
+    </TeachingDefault>
+</template>
